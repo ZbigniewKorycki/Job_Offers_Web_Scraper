c11730460155878bcc7acab643a9b0e19d0d3d57
ZbigniewKorycki <ZbigniewKorycki.info@gmail.com>
2023-08-07 13:48:59 +0200

    Search field send keys ie. "Python"

--- main.py
+++ main.py
@@ -10,8 +10,13 @@ wait = WebDriverWait(driver, 5)
 
 driver.get("https://www.pracuj.pl/")
 
+driver.maximize_window()
+
 cookie_agreement = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '[data-test="button-submitCookie"]')))
 cookie_agreement.click()
+
+search_field = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '[data-test="input-field"]')))
+search_field.send_keys("Python")
 
 
 time.sleep(300)
