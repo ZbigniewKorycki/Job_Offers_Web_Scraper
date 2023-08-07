08f059550c080881de26b6421a4add59d1ec5206
ZbigniewKorycki <ZbigniewKorycki.info@gmail.com>
2023-08-07 13:12:49 +0200

    Add cookie agreement button click

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ driver = webdriver.Chrome()
 
 driver.get("https://www.pracuj.pl/")
 
+cookie_agreement = driver.find_element(By.CSS_SELECTOR, '[data-test="button-submitCookie"]')
+cookie_agreement.click()
 
-time.sleep(5)
+time.sleep(300)
 driver.quit()
+
