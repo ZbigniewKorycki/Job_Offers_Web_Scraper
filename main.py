4198a612df85810d9bcd9e34884a678dfd33f279
ZbigniewKorycki <ZbigniewKorycki.info@gmail.com>
2023-08-07 13:39:16 +0200

    modify cookie agreement button with WebDriverWait and expected_condition

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 
 driver = webdriver.Chrome()
+wait = WebDriverWait(driver, 5)
 
 driver.get("https://www.pracuj.pl/")
 
-cookie_agreement = driver.find_element(By.CSS_SELECTOR, '[data-test="button-submitCookie"]')
+cookie_agreement = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '[data-test="button-submitCookie"]')))
 cookie_agreement.click()
+
 
 time.sleep(300)
 driver.quit()
