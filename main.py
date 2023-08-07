5f99dacdd0e46492550634c151ffb04c68b5b61a
ZbigniewKorycki <ZbigniewKorycki.info@gmail.com>
2023-08-07 12:57:20 +0200

    Add time sleep before quiting

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+import time
 
 driver = webdriver.Chrome()
 
 driver.get("https://www.pracuj.pl/")
+
+
+time.sleep(5)
+driver.quit()
