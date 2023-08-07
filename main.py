e9e622d0672e5f9b53a75c58665d858032550c3d
ZbigniewKorycki <ZbigniewKorycki.info@gmail.com>
2023-08-07 16:17:00 +0200

    add dataframe for offers

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
+import pandas as pd
 
 driver = webdriver.Chrome()
 wait = WebDriverWait(driver, 5)
@@ -36,8 +37,35 @@ print(links)
 
 offer_to_scrape = ["https://www.pracuj.pl/praca/programista-w-dziale-rnd-wroclaw-kazimierza-witalisa-szarskiego-3,oferta,1002772799?s=499ee8a1&searchId=MTY5MTQxNDAwMjU4NC42MDg2"]
 
+company_name = []
+position_name = []
+link_to_offer = []
+localisation = []
+contact_type = []
+experience_level = []
+type_of_work = []
+requirements = []
+good_to_have = []
+
+
+
 for job_offer in offer_to_scrape:
     driver.get(job_offer)
+
+data = pd.DataFrame({"company_name": company_name,
+                     "position_name": position_name,
+                     "link_to_offer": link_to_offer,
+                     "localisation": localisation,
+                     "contact_type": contact_type,
+                     "experience_level": experience_level,
+                     "type_of_work": type_of_work,
+                     "requirements": requirements,
+                     "good_to_have": good_to_have
+                     })
+data.to_csv("job_offers.csv")
+
+offers = pd.read_csv("job_offers.csv")
+print(offers)
 
 time.sleep(300)
 driver.quit()
