514f6083c73b852279b1c107ca203607e5a3defa
ZbigniewKorycki <ZbigniewKorycki.info@gmail.com>
2023-08-07 15:14:12 +0200

    add clicking into multiple localisation offers

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import time
 
 driver = webdriver.Chrome()
 wait = WebDriverWait(driver, 5)
+wait_long = WebDriverWait(driver, 10)
 
 driver.get("https://www.pracuj.pl/")
 
@@ -20,6 +21,20 @@ search_field.send_keys("Python")
 
 search_button = wait.until(EC.element_to_be_clickable((By.XPATH, '//button[contains(text(),"Szukaj")]')))
 search_button.click()
+
+multiple_localisation = wait_long.until(EC.presence_of_all_elements_located((By.XPATH, '//div[@data-test-location="multiple"]')))
+
+for offer_with_multiple_localisation in multiple_localisation:
+    offer_with_multiple_localisation.click()
+
+offers_current_page = wait_long.until(EC.presence_of_all_elements_located((By.XPATH, '//a[@data-test="link-offer"]')))
+links = [offer.get_attribute("href") for offer in offers_current_page]
+
+print(links)
+
+
+# element = wait.until(EC.presence_of_element_located((By.XPATH, '//a[@data-test="link-offer"]')))
+# href_value = element.get_attribute("href")
 
 time.sleep(300)
 driver.quit()
